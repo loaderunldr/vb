bfc544d362d924580b15ba2ff267320326ffda38
nxxnel1ve <nxxnel1ve@gmail.com>
2018-04-22 23:08:19 +0600

    adding tag

--- WindowsApp1/John.vb
+++ WindowsApp1/John.vb
@@ -1,4 +1,6 @@
 ﻿Public Class John
+    ' Please don't steal my code it is useless
+
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
         Me.Hide()
         vb_botz.Show()
